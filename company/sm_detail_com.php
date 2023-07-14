7feb868d9a8674f6c5220f3553f3f761e4ffbda3
Shu4910 <101110626+Shu4910@users.noreply.github.com>
2023-07-14 23:00:46 +0900

    ぴ

--- company/sm_detail_com.php
+++ company/sm_detail_com.php
@@ -2,115 +2,118 @@
 session_start(); // セッションを開始
 
 function loadEnv()
-{
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+{    
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
     $dotenv->load();
 }
 
-
-require 'vendor/autoload.php';
+require '../vendor/autoload.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
+use Twilio\Rest\Client; // 追加
 
 loadEnv();
-
 
 if (isset($_POST['session_id']) && isset($_POST['message_body'])) {
     $session_id = $_POST['session_id'];
     $message_body = $_POST['message_body'];
     $sender_type = $_POST['sender_type'];
 
-    // session_idをcompany_send_idとuser_send_idに分割
     $ids = explode('_', $session_id);
     $company_send_id = $ids[0];
     $user_send_id = $ids[1];
 
     $dbh = new PDO('mysql:dbname=bizdiverse;charset=utf8;host=localhost', 'root', '');
 
-    // SQLを準備
-    $sql = "INSERT INTO messages (session_id, user_send_id, company_send_id, message_body, send_at, sender_type) VALUES (:session_id, :user_send_id, :company_send_id, :message_body, NOW(), :sender_type)";
-
-    // SQLを実行
+    $sql = "INSERT INTO messages (session_id, user_send_id, message_body, send_at, sender_type) VALUES (:session_id, :user_send_id, :message_body, NOW(), :sender_type)";
     $stmt = $dbh->prepare($sql);
     $stmt->bindParam(':session_id', $session_id);
     $stmt->bindParam(':user_send_id', $user_send_id);
-    $stmt->bindParam(':company_send_id', $company_send_id);
     $stmt->bindParam(':message_body', $message_body);
     $stmt->bindParam(':sender_type', $sender_type);
-
     $stmt->execute();
 
-    // 新しく追加されたメッセージのIDを取得
     $last_message_id = $dbh->lastInsertId();
-
-    // last_idカラムに最新のメッセージIDを保存
     $sql = "UPDATE messages SET last_id = :last_id WHERE session_id = :session_id";
     $stmt = $dbh->prepare($sql);
     $stmt->bindParam(':last_id', $last_message_id);
     $stmt->bindParam(':session_id', $session_id);
     $stmt->execute();
 
-    // If last_id was updated (a new message arrived), send a notification email
     if($stmt->rowCount() > 0) {
-        // Fetch the email associated with the user_send_id and company_send_id
-        $sql = "SELECT mail FROM bizdiverse WHERE id = :user_send_id UNION SELECT com_email FROM bizdiverse_company WHERE company_id = :company_send_id";
+        $sql = "SELECT mail, tel FROM bizdiverse WHERE id = :user_send_id";
         $stmt = $dbh->prepare($sql);
         $stmt->bindParam(':user_send_id', $user_send_id);
+        $stmt->execute();
+
+        $contacts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        // Get the company name using the $company_send_id
+        $sql = "SELECT houjin FROM bizdiverse_company WHERE company_id = :company_send_id";
+        $stmt = $dbh->prepare($sql);
         $stmt->bindParam(':company_send_id', $company_send_id);
         $stmt->execute();
 
-        $emails = $stmt->fetchAll(PDO::FETCH_COLUMN, 0); // Extract only the email column
+        $company = $stmt->fetch(PDO::FETCH_ASSOC);
+        $company_name = $company['houjin'];
 
-        $subject = "New_company";
-        $message = "新着メッセージがあります。チャットを確認してください。\n\nメッセージ内容: " . $message_body;
+        $subject = "New_Message(BizDiverse)";
+        $message = "新着メッセージがあります。" . "企業名: " . $company_name . "\n\nメッセージ内容: " . $message_body;
+        
+        $mailer = new PHPMailer(true);
+        try {
+            $mailer->isSMTP();
+            $mailer->Host = $_ENV['SMTP_HOST']; 
+            $mailer->Port = $_ENV['SMTP_PORT']; 
+            $mailer->SMTPAuth = true; 
+            $mailer->Username = $_ENV['SMTP_USER'];
+            $mailer->Password = $_ENV['SMTP_PASS']; 
+            $mailer->setFrom('postmaster@komaki0910.sakura.ne.jp', 'Test');
+            $url = "https://example.com/message_details.php?session_id=$session_id";
+            $message .= "\n\nメッセージやり取りページ: <a href='$url'>$url</a>";
+            $mailer->isHTML(true);
+            $mailer->Subject = $subject;
+            $mailer->Body    = $message;
+            $mailer->AltBody = strip_tags($message);
 
-$mailer = new PHPMailer(true);
-try {
-    // SMTP設定
-    $mailer->isSMTP();
-    $mailer->Host = $_ENV['SMTP_HOST']; // SMTPサーバーのホスト名
-    $mailer->Port = $_ENV['SMTP_PORT']; // SMTPサーバーのポート番号
-    $mailer->SMTPAuth = true; // SMTP認証を使用するかどうか
-    $mailer->Username = $_ENV['SMTP_USER']; // SMTP認証のユーザー名
-    $mailer->Password = $_ENV['SMTP_PASS']; // SMTP認証のパスワード
+            $comEmails = array_filter($contacts, function ($contact) {
+                return filter_var($contact['mail'], FILTER_VALIDATE_EMAIL);
+            });
 
-    // 送信元と宛先の設定
-    $mailer->setFrom('postmaster@komaki0910.sakura.ne.jp', 'Test'); // 送信元メールアドレスと送信者名
+            foreach($comEmails as $contact) {
+                $mailer->addAddress($contact['mail']);
+            }
 
-    // メッセージ内容にURLを添付
-    $url = "https://example.com/message_details.php?session_id=$session_id";
-    $message .= "\n\nメッセージやり取りページ: <a href='$url'>$url</a>";
+            $mailer->send();
+            echo 'Message has been sent';
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mailer->ErrorInfo}";
+        }
 
-    // メール内容
-    $mailer->isHTML(true);
-    $mailer->Subject = $subject;
-    $mailer->Body    = $message;
-    $mailer->AltBody = strip_tags($message);
+        $sid = $_ENV['TWILIO_SID'];
+        $token = $_ENV['TWILIO_AUTH_TOKEN'];
+        $twilio_number = $_ENV['TWILIO_PHONE_NUMBER'];
+        $twilio = new Client($sid, $token);
+    
+        $sms_body = "（BizDiverse）新着メッセージが届いています。";
 
-    // 受信先 (com_email のみを取得)
-    $comEmails = array_filter($emails, function ($email) {
-        return filter_var($email, FILTER_VALIDATE_EMAIL);
-    });
+        $phoneNumbers = array_column($contacts, 'tel');
 
-    foreach($comEmails as $email) {
-        $mailer->addAddress($email);
+        foreach($phoneNumbers as $phoneNumber) {
+            $internationalPhoneNumber = '+81' . substr($phoneNumber, 1);
+            $twilio->messages->create(
+                $internationalPhoneNumber,
+                [
+                    'from' => $twilio_number,
+                    'body' => $sms_body
+                ]
+            );
+        }
     }
-
-    $mailer->send();
-    echo 'Message has been sent';
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mailer->ErrorInfo}";
-}
-
-
-
-
-
-    }
-
     header("Location: message_details.php?session_id=$session_id");
 } else {
     echo "No session_id or message_body specified.";
 }
+?>
