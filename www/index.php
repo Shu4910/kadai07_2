eebf701fb01989b05c5ba87b8dfdfaae06b3c96e
Shu4910 <101110626+Shu4910@users.noreply.github.com>
2023-07-31 22:13:55 +0900

    pi

--- www/index.php
+++ www/index.php
@@ -4,23 +4,21 @@
     <title>ログイン画面</title>
     <!-- ブートストラップのCSSを読み込む -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
 </head>
 <body>
 
 <div class="container mt-5">
     <div class="row">
-        <div class="col-md-12 col-lg-6 mb-3">
+        <div class="col-12 login-container">
             <!-- 個人用ログイン画面のボタン -->
-            <form action="user/index_user.php" method="get">
-                <button class="btn btn-primary btn-lg btn-block" type="submit">個人用ログイン画面</button>
+            <form action="user/index_user.php" method="get" class="mb-3">
+                <button class="btn btn-primary btn-lg login-button" type="submit">個人用ログイン画面</button>
             </form>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-12 col-lg-6">
             <!-- 企業用ログイン画面のボタン -->
             <form action="company/index_com.php" method="get">
-                <button class="btn btn-primary btn-lg btn-block" type="submit">企業用ログイン画面</button>
+                <button class="btn btn-primary btn-lg login-button" type="submit">企業用ログイン画面</button>
             </form>
         </div>
     </div>
--- www/user/dash.php
+++ www/user/dash.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>pipi</title>
+    <title>登録画面</title>
     <!-- ブートストラップのCSSを読み込む -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
 
