2c07a6c7bd385b25b9f46db8c76ec1b6be9e822e
Shu4910 <101110626+Shu4910@users.noreply.github.com>
2023-08-03 13:03:18 +0900

    pi

--- www/index.php
+++ www/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>ログイン 画面</title>
+    <title>ログイン画面</title>
     <link rel="icon" href="img/favicon.ico" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -32,7 +32,7 @@
 <body>
 
 <div class="container mt-5">
-        <div class="service-title">精神障害者特化型<br>スカウトメディアサービス</div> <!-- クラスを追加 -->
+        <div class="service-title">精神障害者a特化型<br>スカウトメディアサービス</div> <!-- クラスを追加 -->
         <h2 class="text-center mb-4">BizDiverse</h2>
 
 
