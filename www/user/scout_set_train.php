8844cf042487ae93544d89d396e08f9799e2dc49
Shu4910 <sz91hs@gmail.com>
2023-08-26 11:35:01 +0900

    pi

--- www/user/scout_set_train.php
+++ www/user/scout_set_train.php
@@ -7,63 +7,6 @@ require '../../database.php';
 $mail = $_SESSION['mail']; // セッションからメールアドレスを取得
 $msg = '';
 
-$prefectureOptions = [
-    "" => "選択してください",
-    "東京都" => "東京都",
-    "神奈川県" => "神奈川県",
-    "埼玉県" => "埼玉県"
-    // 他の都道府県もここに追加
-];
-
-$prefectureCityMapping = [
-    "東京都" => [
-        "千代田区" => "千代田区",
-        "港区" => "港区",
-        "八王子市" => "八王子市",
-        "立川市" => "立川市"
-    ],
-    "神奈川県" => [
-        "川崎市" => "川崎市",
-        "横浜市" => "横浜市"
-    ],
-    "埼玉県" => [
-        "さいたま市" => "さいたま市",
-        "川口市" => "川口市"
-    ],
-    // 他の都道府県と都市のマッピングもここに追加
-];
-
-
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    // ログアウトが押された場合
-    if (isset($_POST['logout'])) {
-        header('Location: scout_set_first.php');
-        exit;
-    }
-
-
-    if (isset($_POST['update'])) {
-        // All the other form fields
-        $newMail = empty($_POST['mail']) ? $mail : $_POST['mail'];
-        $prefecture = $_POST['prefecture'];
-        $cities = is_array($_POST['city']) ? implode(",", $_POST['city']) : $_POST['city'];
-
-        // ここでDBへの更新処理
-        $updateStmt = $pdo->prepare("UPDATE bizdiverse_user 
-        SET mail = :newMail, prefecture = :prefecture, city = :cities 
-        WHERE mail = :mail");
-    $updateStmt->bindValue(':newMail', $newMail, PDO::PARAM_STR);
-    $updateStmt->bindValue(':prefecture', $prefecture, PDO::PARAM_STR);
-    $updateStmt->bindValue(':cities', $cities, PDO::PARAM_STR);
-    $updateStmt->bindValue(':mail', $mail, PDO::PARAM_STR);
-    $updateStmt->execute();
-
-        $_SESSION['mail'] = $newMail; // Update the session email
-        $mail = $newMail; // Update the local email variable
-        $msg = '登録を更新しました。';
-    }
-}
-
 $stmt = $pdo->prepare("SELECT * FROM bizdiverse_user WHERE mail = :mail");
 $stmt->bindValue(':mail', $mail, PDO::PARAM_STR);
 $stmt->execute();
@@ -74,135 +17,22 @@ $userData = $stmt->fetch(PDO::FETCH_ASSOC);
 <!DOCTYPE html>
 <html>
 <head>
-    <title>エリア設定</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <script src="form.js"></script>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>都道府県から沿線/駅表示プルダウン表示</title>
+<script type="text/javascript" src="https://express.heartrails.com/api/express.js"></script>
 </head>
-<body>
-
-<div class="container mt-5">
-    <div class="row justify-content-center">
-        <div class="col-md-5">
-            <h2 class="text-center mb-4">エリア設定</h2>
-            <?php if (!empty($msg)) { echo '<div class="alert alert-danger">' . htmlspecialchars($msg, ENT_QUOTES, 'UTF-8') . '</div>'; } ?>
-            <div class="card">
-                <div class="card-body">
-                    <form method="POST">
-
-                        <!-- 以下が新しく追加した項目です -->
-                        <div id="areaSelection">
-                            <div class="form-group">
-                                <label for="prefecture">都道府県:</label>
-                                <select class="form-control" id="prefecture" name="prefecture">
-                                    <?php
-                                    foreach ($prefectureOptions as $value => $label) {
-                                        $selected = ($userData['prefecture'] === $value) ? 'selected' : '';
-                                        echo '<option value="' . $value . '" ' . $selected . '>' . $label . '</option>';
-                                    }
-                                    ?>
-                                </select>
-                            </div>
-                            <div class="form-group">
-                                <p>都市選択:</p>
-                                <div id="city">
-                                    <?php
-                                    $selectedCities = explode(',', $userData['city']);
-                                    foreach ($prefectureCityMapping as $prefecture => $cities) {
-                                        foreach ($cities as $cityValue => $cityLabel) {
-                                            $checked = in_array($cityValue, $selectedCities) ? 'checked' : '';
-                                            echo '<input type="checkbox" id="' . $cityValue . '" name="city[]" value="' . $cityValue . '" ' . $checked . '>
-                                            <label for="' . $cityValue . '">' . $cityLabel . '</label><br>';
-                                        }
-                                    }
-
-                                    ?>
-                                </div>
-                            </div>
-                        </div>
-                        <button type="submit" name="update" class="btn btn-primary btn-block">更新</button>
-                    </form>
-                    <form method="POST" style="margin-top: 10px;">
-                        <button type="submit" name="logout" class="btn btn-secondary btn-block">戻る</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-    <!-- フッターにサービス名を追加 -->
-    <footer class="text-center mb-4 pt-3">
-    <p>&copy; BizDiverse</p>
-</footer>
-
-<!-- Optional JavaScript -->
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-
-<script>
-
-
-$(document).ready(function(){
-    
-    // 都道府県に基づく都市の情報を更新する関数
-    function updateCityOptions() {
-        var prefecture = $('#prefecture').val();
-
-        // 既に選択されている都市を取得
-        var selectedCities = [];
-        $('input[name="city[]"]:checked').each(function(){
-            selectedCities.push($(this).val());
-        });
-
-        var cityOptions = '';
-        switch (prefecture) {
-            case "東京都":
-                <?php
-                foreach ($prefectureCityMapping["東京都"] as $city => $label) {
-                    echo "cityOptions += '<input type=\"checkbox\" id=\"" . $city . "\" name=\"city[]\" value=\"" . $city . "\" ' + (selectedCities.includes('" . $city . "') ? 'checked' : '') + '> <label for=\"" . $city . "\">" . $label . "</label><br>';\n";
-                }
-                ?>
-                break;
-            case '神奈川県':
-                <?php
-                foreach ($prefectureCityMapping['神奈川県'] as $city => $label) {
-                    echo "cityOptions += '<input type=\"checkbox\" id=\"" . $city . "\" name=\"city[]\" value=\"" . $city . "\" ' + (selectedCities.includes('" . $city . "') ? 'checked' : '') + '> <label for=\"" . $city . "\">" . $label . "</label><br>';\n";
-                }
-                ?>
-                break;
-
-            case '埼玉県':
-                <?php
-                foreach ($prefectureCityMapping['埼玉県'] as $city => $label) {
-                    echo "cityOptions += '<input type=\"checkbox\" id=\"" . $city . "\" name=\"city[]\" value=\"" . $city . "\" ' + (selectedCities.includes('" . $city . "') ? 'checked' : '') + '> <label for=\"" . $city . "\">" . $label . "</label><br>';\n";
-                }
-                ?>
-                break;
-
-
-            // 他の都道府県の場合もここに追加
-            default:
-                cityOptions = ''; // 何も選択されていない場合は都市のオプションを空にする
-                break;
-        }
-        $('#city').html(cityOptions);
-    }
-    
-    // 都道府県が変更されたときのイベントハンドラ
-    $('#prefecture').change(function(){
-        updateCityOptions();
-    });
-    
-    // ページが読み込まれたときに関数を呼び出して都市の情報を初期化
-    updateCityOptions();
-
-
-});
-
-
-</script>
+<body onload="HRELoadPrefecture('prefecture', 'line', 'station');">
+<p>都道府県の、希望する駅・路線を入力してください。路線のみ記入でも可能です。</p>
+        <select id="prefecture" name="prefecture" onchange="HREOnChangePrefecture();">
+            <option value="">都道府県を選択してください</option>
+        </select>
+        <select id="line" name="line" onchange="HREOnChangeLine();">
+        <option value="路線を選択してください">路線を選択してください</option>
+        </select>
+        <select id="station" name="station">
+        <option value="駅を選択してください">駅を選択してください</option>
+        </select>
 
 </body>
+
 </html>
